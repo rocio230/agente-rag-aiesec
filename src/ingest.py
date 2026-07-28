95d40911863af72c46aad406db380663542599ad
Rocío Ramos <rocyramos19@gmail.com>
2026-07-27 21:45:22 -0300

    feat: add document loading and text splitting pipeline

---
+++ src/ingest.py
@@ -0,0 +1,19 @@
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+def load_documents():
+
+    loader = PyPDFLoader(
+        "data/AIESEC_Agente_RAG_Documentacion.pdf"
+    )
+
+    documents = loader.load()
+
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000,
+        chunk_overlap=200
+    )
+
+    chunks = splitter.split_documents(documents)
+
+    return chunks
